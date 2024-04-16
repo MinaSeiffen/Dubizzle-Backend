e10c84fde8ed4de2e9f9e93b2ec348c76f0fba69
MinaE.Seiffen <160426186+MinaSeiffen@users.noreply.github.com>
2024-04-16 21:22:11 +0200

    Update products.js

--- Routes/products.js
+++ Routes/products.js
@@ -20,9 +20,10 @@ router.get("/search/:subCategoryName", getProductsBySearchInProperties);
 router.get("/getUserAds/:userId", getSellerAds);
 router.get("/getbysubcateboryname", getProdBySub_CategoryName)
 router.get("/get", getAllProducts);
+router.get("/getmyadds", verifyJWT, getMyAdds)
 router.post("/add", verifyJWT, postProduct);
 router.patch("/:id", updateProduct);
-router.delete("/:id", deleteProduct);
+router.delete("/delete/:id", verifyJWT ,deleteProduct);
 router.delete("/userProducts/:idSeller", deleteAllProductWithIdSeller);
 
 module.exports = router;
